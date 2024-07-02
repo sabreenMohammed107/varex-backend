ee658a7892fcf6a3f37ef1f6086b499238c301b5
ahmdsydysf <a.sayed.xc@gmail.com>
2024-07-02 15:46:41 +0300

    v2

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -78,8 +78,8 @@
         }
         @media (min-width: 1600px) {
             .ltn__slide-item-3-normal {
-                min-height: 546px;
-                max-height: 546px;
+                min-height: 566px;
+                max-height: 566px;
             }
         }
     </style>
